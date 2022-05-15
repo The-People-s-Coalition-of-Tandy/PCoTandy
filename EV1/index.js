69392dae28949969e46b28fd2f808101d335f3e6
julie.wav <humphreythegray@gmail.com>
2022-05-15 13:33:11 -0400

    IndexAlt now main index, refactoring

--- EV1/index.js
+++ EV1/index.js
@@ -1,72 +1,197 @@
 import * as THREE from 'three';
-import  {OrbitControls} from "https://unpkg.com/three@0.128.0/examples/jsm/controls/OrbitControls.js";
-import  {GLTFLoader} from "https://unpkg.com/three@0.128.0/examples/jsm/loaders/GLTFLoader.js";
+import {
+    OrbitControls
+} from "https://unpkg.com/three@0.128.0/examples/jsm/controls/OrbitControls.js";
+import {
+    GLTFLoader
+} from "https://unpkg.com/three@0.128.0/examples/jsm/loaders/GLTFLoader.js";
+import {
+    RGBELoader
+} from "https://unpkg.com/three@0.128.0/examples/jsm/loaders/RGBELoader.js";
 
 let camera, scene, renderer;
+let logoMixer, singerOneMixer, singerTwoMixer, singerThreeMixer;
+let startSinging = false;
+let clock = new THREE.Clock();
+var raycaster, mouse;
+let bits = 30;
+
+
 
 init();
 render();
 
 function init() {
-// The three main things! Scene, camera, render
-const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xffffff );
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 1000);
+    camera.position.set(0, 0, 10);
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-camera.position.set(0, 0, 2);
-camera.lookAt(0, 0, 0);
+    scene = new THREE.Scene();
+    raycaster = new THREE.Raycaster();
+    mouse = new THREE.Vector2()
+    // scene.background = new THREE.Color(0xffffff);
 
-const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#bg')}); // {alpha:true} for transparency
-// renderer.setClearColorHex( 0xffffff, 1 ); // assumes black background
-renderer.setSize(window.innerWidth, window.innerHeight);
-// renderer.setSize(window.innerWidth/2, window.innerHeight/2, false); //makes it low res!
+    // model
 
-document.body.appendChild(renderer.domElement); // This is a <canvas> element
+    new RGBELoader()
+        .setPath('./assets/')
+        .load('royal_esplanade_1k.hdr', function (texture) {
+            texture.mapping = THREE.EquirectangularReflectionMapping;
+
+            scene.environment = texture;
+
+            const loader = new GLTFLoader().setPath('./assets/');
+            loader.load('BoomBox.glb', function (gltf) {
+                gltf.scene.scale.set(120, 120, 120)
+                // gltf.scene.scale.set(160, 600, 160) // weird tall boombox
+                gltf.scene.position.set(0, -2, 0);
+                scene.add(gltf.scene);
+
+                render();
+
+            });
+
+            loader.load('tandyLogoSpin.glb', function (gltf) {
+                gltf.scene.scale.set(.15, .15, .15);
+                gltf.scene.position.set(-.3, 2.5, 0);
+                logoMixer = new THREE.AnimationMixer(gltf.scene);
+                gltf.animations.forEach((clip) => {
+                    logoMixer.clipAction(clip).play();
+                });
+
+                gltf.scene.remove(gltf.scene.children[0]);
+                scene.add(gltf.scene);
+
+                render(scene, camera);
+
+            });
+
+            loader.load('16untimed.glb', function (gltf) {
+                gltf.scene.scale.set(1.3, 1.3, 1.3);
+                gltf.scene.position.set(6.5, .4, 1);
+                singerOneMixer = new THREE.AnimationMixer(gltf.scene);
+                gltf.animations.forEach((clip) => {
+                    singerOneMixer.clipAction(clip).play();
+                });
+                scene.add(gltf.scene);
+
+                render(scene, camera);
+            });
+
+            loader.load('16untimed.glb', function (gltf) {
+                gltf.scene.scale.set(1.3, 1.3, 1.3);
+                gltf.scene.position.set(1.5, .4, -7);
+                gltf.scene.rotation.set(0,1.57,0);
+                singerTwoMixer = new THREE.AnimationMixer(gltf.scene);
+                gltf.animations.forEach((clip) => {
+                    singerTwoMixer.clipAction(clip).play();
+                });
+                scene.add(gltf.scene);
+
+                render(scene, camera);
+            });
+
+            loader.load('16untimed.glb', function (gltf) {
+                gltf.scene.scale.set(1.3, 1.3, 1.3);
+                gltf.scene.position.set(1.75, .8, 2.5);
+                gltf.scene.rotation.set(-.5,-.3,-.5);
+                singerThreeMixer = new THREE.AnimationMixer(gltf.scene);
+                gltf.animations.forEach((clip) => {
+                    singerThreeMixer.clipAction(clip).play();
+                });
+                scene.add(gltf.scene);
+
+                render(scene, camera);
+            });
+
+        });
 
 
-// NOTE: for low res, which is not used to my knowledge yet !!
-// If you wish to keep the size of your app but render it at a lower resolution, 
-// you can do so by calling setSize with false as updateStyle (the third argument). 
-// For example, setSize(window.innerWidth/2, window.innerHeight/2, false) 
-// will render your app at half resolution, given that your <canvas> has 100% width and height.
+    var light = new THREE.DirectionalLight(0xfdfdfd, .5);
+    light.position.set(2, 2, 1).normalize();
+    var ambientLight = new THREE.AmbientLight(0x000022);
+    scene.add(light);
+    scene.add(ambientLight);
 
-const loader = new GLTFLoader().setPath( './assets/' );
-						loader.load( 'BoomBox.glb', function ( gltf ) {
-                            gltf.scene.scale.set(40,40,40);
-							scene.add( gltf.scene );
+    renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        canvas: document.querySelector('#bg'),
+        alpha: true
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth / bits, window.innerHeight / bits, false); 
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.toneMappingExposure = 5;
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    document.body.appendChild(renderer.domElement);
 
-							render(scene, camera);
+    const controls = new OrbitControls(camera, renderer.domElement);
+    // controls.addEventListener('change', render); // use if there is no animation loop
+    // controls.minDistance = 2;
+    // controls.maxDistance = 10;
+    controls.target.set(0, 0, -0.2);
+    controls.update();
 
-						} );
+    window.addEventListener('resize', onWindowResize);
+    renderer.domElement.addEventListener('click', onClick, false);
 
-                        loader.load( 'tandyLogoSpin.glb', function ( gltf ) {
-                            gltf.scene.scale.set(.08,.08,.08);
-							scene.add( gltf.scene );
 
-							render(scene, camera);
-
-						} );
-						const controls = new OrbitControls( camera, renderer.domElement );
-				// controls.addEventListener( 'change', renderer.render ); // use if there is no animation loop
-				controls.minDistance = 2;
-				controls.maxDistance = 10;
-				controls.target.set( 0, 0, - 0.2 );
-				controls.update();
 }
 
 function onWindowResize() {
 
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-	renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
-	render();
+function onClick() {
 
+    event.preventDefault();
+
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+
+    var intersects = raycaster.intersectObjects(scene.children, true);
+
+    if (intersects.length > 0 & intersects[0].object.name === "BoomBox") {
+        startSong()
+    }
+
+}
+
+function startSong() {
+    if (!startSinging) {
+        document.getElementById('EV1').play();
+        setTimeout(() => {
+            startSinging = true;
+        }, 13500);
+    }
 }
 
 function render() {
 
-	renderer.render( scene, camera );
+    var delta = clock.getDelta();
+    logoMixer.update(delta);
+    if (startSinging) {
+        singerOneMixer.update(delta);
+        singerTwoMixer.update(delta);
+        singerThreeMixer.update(delta);
+    }
+
+    requestAnimationFrame(render);
+    renderer.setSize(window.innerWidth / bits, window.innerHeight / bits, false); 
+    if(bits>1){
+        if(bits>15){
+            bits-=.1
+        }
+        else {
+            bits -=0.2;
+        }
+    }
+    renderer.render(scene, camera);
 
 }
---
+++ ad.js
@@ -0,0 +1,52 @@
+document.write(`
+<!-- popup ad -->
+
+  <!-- 1# top popup -->
+  <!-- animation info -->
+  <div class="glx-slider-container-18000"
+    style="max-width: 360px; box-sizing: border-box; padding: 12px; position: fixed; display: flex; inset: 0px 0px auto auto; flex-direction: column; justify-content: flex-start; z-index: 2147483647;">
+    <div id="glx-18000-container"
+      style="display: flex; flex-flow: column nowrap; box-sizing: border-box; visibility: visible; clear: both;">
+
+      <!-- #1 link -->
+      <a href="./catalog.html" class="glx-teaser visible"
+        style="display: flex; box-sizing: border-box; flex-flow: row nowrap; max-width: 100%; max-height: 88px; margin-bottom: 12px;">
+
+        <!-- #1 image thumbnail -->
+        <img src="./Assets/PCoTandyLogo.png" g-role="img" class="glx-image"
+          style="width: 15%; height: 47px; object-fit: cover; flex-grow: 0; flex-shrink: 0; box-sizing: border-box;">
+
+        <div g-role="text" class="glx-text"
+          style="width: 85%; display: flex; flex-direction: column; overflow: hidden; box-sizing: border-box; max-height: 47px;">
+
+          <!-- #1 Text -->
+          <div g-role="header" class="glx-header">New Releases</div>
+          <div g-role="description" class="glx-description">♥ Sam Walters ♥</div>
+        </div>
+
+        <!-- ok/close dismiss buttons -->
+        <div g-role="ctrls" class="glx-ctrls"><button class="glx-ctrls-button glx-cls" g-role="cls">Listen</button>
+          <button class="glx-ctrls-button glx-ok" g-role="ok">Close</button>
+          <!-- link when OK is pressed ... vestigial from when this was predatory -->
+        </div>
+
+        <!-- #2 Link -->
+        <a href="./annualReport.html" class="glx-teaser visible"
+          style="display: flex; box-sizing: border-box; flex-flow: row nowrap; max-width: 100%; max-height: 88px; margin-bottom: 12px;"><img
+            src="./Assets/GoogleFingers.gif" g-role="img" class="glx-image"
+            style="width: 15%; height: 47px; object-fit: cover; flex-grow: 0; flex-shrink: 0; box-sizing: border-box;">
+          <div g-role="text" class="glx-text"
+            style="width: 85%; display: flex; flex-direction: column; overflow: hidden; box-sizing: border-box; max-height: 47px;">
+
+            <!-- #2 Text -->
+            <div g-role="header" class="glx-header">Sync your tetso cube NOW!</div>
+            <div g-role="description" class="glx-description">Firmware upgrade 1.2A will be required</div>
+          </div>
+          <!-- #2 buttons -->
+          <div g-role="ctrls" class="glx-ctrls"><button class="glx-ctrls-button glx-cls" g-role="cls">Sync</button>
+            <button class="glx-ctrls-button glx-ok" g-role="ok">Remind Me</button>
+          </div>
+          <a href="" class="glx-main-link"></a>
+        </a></div>
+  </div>
+`)
