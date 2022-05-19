bc2ccfea25de67815f1dbfe81f905eca0a6f7cef
Julie Bodian <julia.bodian@vistaprint.com>
2022-05-19 00:45:03 -0400

    Final homepage

--- ad.js
+++ ad.js
@@ -8,7 +8,7 @@ document.write(`
       style="display: flex; flex-flow: column nowrap; box-sizing: border-box; visibility: visible; clear: both;">
 
       <!-- #1 link -->
-      <a href="./catalog.html" class="glx-teaser visible"
+      <a class="glx-teaser visible"
         style="display: flex; box-sizing: border-box; flex-flow: row nowrap; max-width: 100%; max-height: 88px; margin-bottom: 12px;">
 
         <!-- #1 image thumbnail -->
@@ -16,34 +16,35 @@ document.write(`
           style="width: 15%; height: 47px; object-fit: cover; flex-grow: 0; flex-shrink: 0; box-sizing: border-box;">
 
         <div g-role="text" class="glx-text"
-          style="width: 85%; display: flex; flex-direction: column; overflow: hidden; box-sizing: border-box; max-height: 47px;">
+          style="width: 85%; display: flex; flex-direction: column; box-sizing: border-box; max-height: 47px;">
 
           <!-- #1 Text -->
-          <div g-role="header" class="glx-header">New Releases</div>
-          <div g-role="description" class="glx-description">♥ Sam Walters ♥</div>
+          <div g-role="header" class="glx-header">New Release May 18th </div>
+          <div g-role="description" class="glx-description">Tetso Cube Lightride - Elie M-H</div>
         </div>
 
         <!-- ok/close dismiss buttons -->
-        <div g-role="ctrls" class="glx-ctrls"><button class="glx-ctrls-button glx-cls" g-role="cls">Listen</button>
-          <button class="glx-ctrls-button glx-ok" g-role="ok">Close</button>
+        <div g-role="ctrls" class="glx-ctrls"><button class="glx-ctrls-button glx-cls" g-role="cls" onclick="location.href='https://tclr.pcotandy.org';">Viz</button>
+          <button class="glx-ctrls-button glx-ok" g-role="ok" onclick="location.href='https://tandy.bandcamp.com/album/tetso-cube-lightride';">Bandcamp</button>
           <!-- link when OK is pressed ... vestigial from when this was predatory -->
         </div>
 
         <!-- #2 Link -->
-        <a href="./annualReport.html" class="glx-teaser visible"
+        <a class="glx-teaser visible"
           style="display: flex; box-sizing: border-box; flex-flow: row nowrap; max-width: 100%; max-height: 88px; margin-bottom: 12px;"><img
             src="./Assets/GoogleFingers.gif" g-role="img" class="glx-image"
             style="width: 15%; height: 47px; object-fit: cover; flex-grow: 0; flex-shrink: 0; box-sizing: border-box;">
           <div g-role="text" class="glx-text"
-            style="width: 85%; display: flex; flex-direction: column; overflow: hidden; box-sizing: border-box; max-height: 47px;">
+            style="width: 85%; display: flex; flex-direction: column; box-sizing: border-box; max-height: 47px;">
 
             <!-- #2 Text -->
             <div g-role="header" class="glx-header">Sync your tetso cube NOW!</div>
             <div g-role="description" class="glx-description">Firmware upgrade 1.2A will be required</div>
           </div>
           <!-- #2 buttons -->
-          <div g-role="ctrls" class="glx-ctrls"><button class="glx-ctrls-button glx-cls" g-role="cls">Sync</button>
-            <button class="glx-ctrls-button glx-ok" g-role="ok">Remind Me</button>
+          <div g-role="ctrls" class="glx-ctrls">
+          <button class="glx-ctrls-button glx-cls" g-role="cls" onclick="location.href='https://pcotandy.org/signup';">Sync</button>
+            <!--<button class="glx-ctrls-button glx-ok" g-role="ok">Remind Me</button> -->
           </div>
           <a href="" class="glx-main-link"></a>
         </a></div>
