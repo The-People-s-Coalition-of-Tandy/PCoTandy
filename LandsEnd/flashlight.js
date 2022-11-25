81dd14451182c8c15c1605f06b6a197f32d20699
julie.wav <humphreythegray@gmail.com>
2022-11-24 22:52:16 -0500

    flashlight version of land's end linx

---
+++ LandsEnd/flashlight.js
@@ -0,0 +1,20 @@
+var rect = document.getElementById("main").getBoundingClientRect();
+
+function update(e) {
+  var x = e.clientX - rect.left; //x position within the element.
+  var y = e.clientY - rect.top; //y position within the element.
+
+  document.documentElement.style.setProperty("--cursorX", x + "px");
+  document.documentElement.style.setProperty("--cursorY", y + "px");
+}
+
+function remove(e) {
+  document.documentElement.style.setProperty("--cursorX", "0px");
+  document.documentElement.style.setProperty("--cursorY", "0px");
+}
+
+var main = document.getElementById("main");
+
+main.addEventListener("mousemove", update);
+main.addEventListener("mouseout", remove);
+main.addEventListener("touchmove", update);
